d69591c37a87b8fcaeceb2c0eb6b6295db9b4470
Christopher Chan <christopherchannn@gmail.com>
2025-03-31 12:12:33 -0700

    changed to login success

--- src/auth.js
+++ src/auth.js
@@ -43,14 +43,13 @@ const msalConfig = {
   };
 
   msalInstance.addEventCallback((message) => {
-    if (message.eventType === msal.EventType.ACCOUNT_ADDED) {
+    if (message.eventType === msal.EventType.LOGIN_SUCCESS) {
         console.log("Account added:", message.payload);
     } else if (message.eventType === msal.EventType.ACCOUNT_REMOVED) {
         console.log("Account removed:", message.payload);
     } else if (message.eventType === msal.EventType.ACTIVE_ACCOUNT_CHANGED) {
         const accountInfo = msalInstance.getActiveAccount();
         console.log("Active account changed:", accountInfo);
-        window.location.replace("/loggedIn.html");
     }
 });
   
